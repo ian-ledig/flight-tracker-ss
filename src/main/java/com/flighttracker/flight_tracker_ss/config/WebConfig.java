8821cdcf7ee86fd1c820933fbb29eae60ddba756
Ian Ledig <ian.ledigfr@gmail.com>
2025-06-08 22:21:51 +0900

    editing CORS

--- src/main/java/com/flighttracker/flight_tracker_ss/config/WebConfig.java
+++ src/main/java/com/flighttracker/flight_tracker_ss/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://localhost:3002")
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("*");
     }
